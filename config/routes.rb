7c9a1e8235e0ee9ba8bb9643145aaf0d0c2cbd57
Tobi Weinstock <tv.weinstock@gmail.com>
2014-08-31 18:57:06 -0400

    Adding flash alterts and notices

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  # get 'sessions/new'
+  resources :products do
+    resources :reviews, only: [:show, :create, :destroy]
+  end
 
-  # get 'sessions/create'
-
-  # get 'sessions/destroy'
-
-  # get 'users/new'
-
-  # get 'users/create'
-
-  resources :products
+  resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
