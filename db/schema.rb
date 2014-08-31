e905b6c63a0331d7782ec5225de458323e31d721
Tobi Weinstock <tv.weinstock@gmail.com>
2014-08-31 15:10:08 -0400

    Creating user model, controller and view

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140829204541) do
+ActiveRecord::Schema.define(version: 20140831155255) do
 
   create_table "products", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.integer  "price_in_cents"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest_string"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
