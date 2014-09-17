f3adfcb0ed9e67d457cca5fda6e791a4048e441f
Tobi Weinstock <tv.weinstock@gmail.com>
2014-09-16 23:04:17 -0400

    move products into a partial

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,53 +2,57 @@ class ProductsController < ApplicationController
   before_action :ensure_logged_in, except: [:index, :show]
 
   def index
-    @products = Product.all
-  end
+    @products = if params[:search]
+      Product.where("LOWER(name) LIKE LOWER(?)", "%#{params[:search]}%")
+      else
+        Product.all
+      end
+    end
 
-  def show
-    @product = Product.find(params[:id])
+    def show
+      @product = Product.find(params[:id])
 
-    if current_user
-      @review = @product.reviews.build
+      if current_user
+        @review = @product.reviews.build
+      end
+    end
+
+    def new
+      @product = Product.new
+    end
+
+    def edit
+      @product = Product.find(params[:id])
+    end
+
+    def create
+      @product = Product.new(product_params)
+
+      if @product.save
+        redirect_to products_url
+      else
+        render :new
+      end
+    end
+
+    def update
+      @product = Product.find(params[:id])
+
+      if @product.update_attributes(product_params)
+        redirect_to product_path(@product)
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @product = Product.find(params[:id])
+      @product.destroy
+      redirect_to products_path
+    end
+
+    private
+    def product_params
+      params.require(:product).permit(:name, :description, :price_in_cents)
     end
   end
-
-  def new
-    @product = Product.new
-  end
-
-  def edit
-    @product = Product.find(params[:id])
-  end
-
-  def create
-    @product = Product.new(product_params)
-
-    if @product.save
-      redirect_to products_url
-    else
-      render :new
-    end
-  end
-
-  def update
-    @product = Product.find(params[:id])
-
-    if @product.update_attributes(product_params)
-      redirect_to product_path(@product)
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
-    redirect_to products_path
-  end
-
-  private
-  def product_params
-    params.require(:product).permit(:name, :description, :price_in_cents)
-  end
-end
